4992ef119278a949eb89dc9f119d3ca5098df775
Marco Santos <101365429+mscode98@users.noreply.github.com>
2022-07-13 05:22:31 +0100

    qoi-viewer

---
+++ main.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "raylib.h"
+
+#define QOI_IMPLEMENTATION
+#include "qoi.h"
+
+int check_qoi_get_channels(const char* filepath)
+{
+    FILE *file = fopen(filepath, "r");
+    if (file == NULL)
+    {
+        printf("ERROR: Couldn't read file %s\n", filepath);
+        exit(1);
+    }
+
+    char *magic = malloc(sizeof(char) * 4); 
+    fseek(file, 0, SEEK_SET);
+    fread(magic, sizeof(char), 4, file);
+
+    if(strcmp(magic, "qoif") != 0) {
+        printf("ERROR: Given file isn't a valid QOI image.\n");
+        exit(1);
+    }
+
+    free(magic);
+
+    char *channels = malloc(sizeof(char)); 
+    fseek(file, 12, SEEK_SET);
+    fread(channels, sizeof(char), 1, file);
+
+    int ret = atoi(channels);
+
+    free(channels);
+    fclose(file);
+
+    return ret;
+}
+
+int main(int argc, char **argv)
+{
+    (void)argc;
+    (void)*argv++;
+
+    if (*argv == NULL)
+    {
+        printf("USAGE: ./qoi-viewer <input>.qoi\n");
+        exit(1);
+    }
+
+    const char *filepath = *argv++;
+
+    int channels = check_qoi_get_channels(filepath);
+
+    qoi_desc desc = {0};
+
+    unsigned char *qoi = qoi_read(filepath, &desc, channels);
+
+    if (qoi == NULL)
+    {
+        printf("ERROR: Couldn't read QOI image.\n");
+        exit(1);
+    }
+
+    InitWindow(desc.width, desc.height, "qoi-viewer");
+    SetWindowTitle(filepath);
+
+    TraceLog(LOG_INFO, "QOI Desc: %dx%d, channels %d", desc.width, desc.height, desc.channels);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        unsigned int y = 0;
+        unsigned int x = 0;
+
+        size_t amount = (desc.width * desc.height) * desc.channels;
+
+        unsigned int pixel = 0;
+        for (size_t i = 0; i < amount;)
+        {
+            unsigned char r = qoi[i++];
+            unsigned char g = qoi[i++];
+            unsigned char b = qoi[i++];
+            unsigned char a = 255;
+
+            if (desc.channels == 4)
+                a = qoi[i++];
+
+            x = pixel % desc.width;
+            y = (pixel - x) / desc.width;
+
+            DrawPixel(x, y, (Color){r, g, b, a});
+
+            pixel++;
+        }
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+}
---
+++ readme.md
@@ -0,0 +1,21 @@
+# qoi-viewer
+
+QOI Image viewer using qoi default implementation and Raylib.
+
+## How to:
+
+Requires CMake.
+
+```console
+git clone https://github.com/mscode98/qoi-viewer.git
+cd qoi-viewer
+cmake .
+make
+./qoi-viewer <input>.qoi
+```
+
+## Copyright:
+
+- Images: Takens from QOI format [website](https://qoiformat.org/)
+- [Raylib](https://github.com/raysan5/raylib)
+- [qoi.h](https://github.com/phoboslab/qoi) the reference encoder and decoder written by the author of QOI.
